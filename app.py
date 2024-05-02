b7f427c9f691ab8e59fa92cc52d88025a6f6e1bc
ShahanaTK <shiyya2002@gmail.com>
2024-05-02 10:19:41 +0530

    Text Classification

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import joblib
+
+
+model=joblib.load("news-classification-model.pkl")
+
+news_labels={0:"Technical",1:"Business",2:"Sports",3:"Entertainment",4:"Politics"}
+
+st.title("News Classification")
+user_input=st.text_area("Enter news here:")
+if st.button("Predict"):
+    predicted_label=model.predict([user_input])[0]
+    #print(predict_sen)
+    predicted_news_label=news_labels[predicted_label]
+    st.info(f"Predicted Sentiment: {predicted_news_label}")
